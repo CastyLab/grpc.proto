145cc02d4b7783b4598629f6500304694db54183
MrJosh <josheghani.dev@gmail.com>
2020-03-16 13:09:46 +0330

    Fixed user.service typo messages package

--- user.service.pb.go
+++ user.service.pb.go
@@ -11,7 +11,7 @@ import (
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
 	math "math"
-	messages "messages"
+	messages "github.com/CastyLab/grpc.proto/messages"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
